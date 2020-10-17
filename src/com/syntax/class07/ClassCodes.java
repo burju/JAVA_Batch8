77fd6387eaee51158576de0a83db7b6714b3a0cc
*Burju <*kanliska@gmail.com>
2020-10-17 12:41:26 -0500

    Codes written in lecture

--- src/com/syntax/class07/ClassCodes.java
+++ src/com/syntax/class07/ClassCodes.java
@@ -1,8 +1,30 @@
 package com.syntax.class07;
 
+import java.util.Scanner;
+
+import com.sun.tools.jdeprscan.scan.Scan;
+
 public class ClassCodes {
 	public static void main(String[] args) {
+	
+			
+			boolean workday= true;
+			int dayNumber= 1;
 		
+			
+			while ( workday){
+				System.out.println("I need a day off");
+				dayNumber++;
+				if(dayNumber>=6) {
+					workday=false;}
+				
+					while( !workday && dayNumber<8) {
+					System.out.println("I dont need a day off anymore");
+					dayNumber++;
+					
+				}
+			}
+				
+			}
 	}
 
-}
